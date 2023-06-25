dde2ce55eb657bda300318d2ba47445e5551b854
PMagatteS <papamagattesady@gmail.com>
2023-06-25 18:04:16 +0000

    Added several methods from the HUD

--- Main.gd
+++ Main.gd
@@ -6,20 +6,24 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_player_hit():
-	pass # Replace with function body.
+	game_over()
 
 
 func _on_start_timer_timeout():
@@ -30,6 +34,7 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_mob_timer_timeout():
