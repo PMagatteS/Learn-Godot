309fb39957e935a4d4ad0ae3d4a52976fe0cbed2
PMagatteS <papamagattesady@gmail.com>
2023-06-26 11:00:50 +0000

    Added coin's logic all over the script

--- Main.gd
+++ Main.gd
@@ -2,7 +2,11 @@ extends Node
 
 # This allow us to choose the Mob scene we want to instance
 @export var mob_scene: PackedScene
+@export var coin_scene: PackedScene
+
 var score
+var screenWidth = ProjectSettings.get_setting("display/window/size/viewport_width")
+var screenHeight = ProjectSettings.get_setting("display/window/size/viewport_height")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,13 +16,16 @@ func _ready():
 func game_over():
 	$Music.stop()
 	$DeathSound.play()
+	$CoinTimer.stop()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	
 
 func new_game():
 	$Music.play()
 	get_tree().call_group("mobs", "queue_free")
+	get_tree().call_group("coins", "queue_free")	
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
@@ -30,6 +37,7 @@ func _on_player_hit():
 
 
 func _on_start_timer_timeout():
+	$CoinTimer.start()
 	$MobTimer.start()
 	$ScoreTimer.start()
 	
@@ -42,17 +50,23 @@ func _on_score_timer_timeout():
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
+	
 	var mob = mob_scene.instantiate()
 
+	
+	
 	# Choose a random location on Path2D.
 	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
 	mob_spawn_location.progress_ratio = randf()
+	
 
 	# Set the mob's direction perpendicular to the path direction.	 
 	var direction = mob_spawn_location.rotation + PI / 2
 
 	# Set the mob's position to a random location.
 	mob.position = mob_spawn_location.position
+	
+	
 
 	# Add some randomness to the direction.
 	direction += randf_range(-PI / 4, PI / 4)
@@ -64,3 +78,15 @@ func _on_mob_timer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+
+
+func _on_player_coin_collected():
+	score += 5 
+	$HUD.update_score(score)
+
+
+func _on_coin_timer_timeout():
+	var coin = coin_scene.instantiate()
+	coin.position = Vector2(randf_range(0.0, screenWidth), randf_range(0.0, screenHeight))
+	add_child(coin)
+
