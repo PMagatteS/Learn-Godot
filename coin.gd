172ca1ff4be0acdd1712eb8340367ca8059d0185
PMagatteS <papamagattesady@gmail.com>
2023-06-30 21:24:05 +0000

    Added collected method and discover Tweens

--- coin.gd
+++ coin.gd
@@ -4,7 +4,17 @@ func _ready():
 	$AnimatedSprite2D.play("default")
 	$DespawnTimer.start()
 	
-
-
+	
+	
 func _on_despawn_timer_timeout():
-	queue_free()
+	var disappear = get_tree().create_tween()
+	disappear.tween_property(self, "modulate:a", 0, .25)
+	disappear.tween_callback(queue_free)
+
+func collected():
+	var moveUp = get_tree().create_tween()
+	var disappear = get_tree().create_tween()
+	moveUp.tween_property(self, "position", position-Vector2(0, 20), .25)
+	disappear.tween_property(self, "modulate:a", 0, .25)
+	moveUp.tween_callback(queue_free)
+
